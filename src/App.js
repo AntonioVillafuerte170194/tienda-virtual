8527bbfed6ec023ccb2e37a12b010453182233af
antoniovillafuerte170194 <villafuerteantonio74@gmail.com>
2023-04-18 16:22:49 -0300

    added slider and pay

--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
 import './App.css';
+import Pay from './components/Pay';
+import Slide from './components/Slider';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Slide></Slide>
+      <Pay></Pay>
     </div>
   );
 }
---
+++ src/components/Pay/index.jsx
@@ -0,0 +1,62 @@
+import React, { useState} from "react";
+import { initMercadoPago } from "@mercadopago/sdk-react";
+import Payment from "./Payment";
+import Checkout from "./Checkout";
+import Footer from "./Footer";
+import './pay.css';
+import InternalProvider from "./ContextProvider";
+import { SpinnerCircular } from 'spinners-react';
+
+// REPLACE WITH YOUR PUBLIC KEY AVAILABLE IN: https://developers.mercadopago.com/panel
+initMercadoPago("TEST-34c0de2c-9043-4f34-b535-438550fad9eb");
+
+const App = () => {
+  const [preferenceId, setPreferenceId] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [orderData, setOrderData] = useState({ quantity: "1", price: "10", amount: 10, description: "Some book" });
+  
+  const handleClick = () => {
+    setIsLoading(true);
+    fetch("http://localhost:8080/create_preference", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(orderData),
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((preference) => {
+        setPreferenceId(preference.id);
+      })
+      .catch((error) => {
+        console.error(error);
+      }).finally(() => {
+        setIsLoading(false);
+      })
+  };
+
+  const renderSpinner = () => {
+     if (isLoading) {
+      return (
+        <div className="spinner-wrapper">
+          <SpinnerCircular сolor='#009EE3' />
+        </div>
+      )
+     }
+  }
+
+  return (
+    <InternalProvider context={{ preferenceId, isLoading, orderData, setOrderData }}>
+      <main>
+        {renderSpinner()}
+        <Checkout onClick={handleClick} description/>
+        <Payment />
+      </main>
+      {/* <Footer /> */}
+    </InternalProvider>
+  );
+};
+
+export default App;
